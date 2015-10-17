550150353025ffb675c3c25e4c6eb6b017643337
Tommy Parnell <tparnell8@gmail.com>
2015-10-16 21:40:51 -0400

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var S = require('string');
+var app = express();
+
+
+app.get('/*', function (req, res) {
+  var host = S(req.headers.host);
+  
+  if(host.contains('github')){
+    return res.redirect(301, 'https://github.com/tparnell8');
+  }
+  if(host.contains('blog')){
+    return res.redirect(301,'http://blog.tparnell.io');
+  }
+  if(host.contains('resume')){
+  return res.redirect(301,'http://resume.tparnell.io');
+  }
+  res.end();
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
